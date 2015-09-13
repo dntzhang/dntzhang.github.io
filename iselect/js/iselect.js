27bd75e92311bc04f5faf3feec2d56a4a202b3df
张磊 <kmdjs@qq.com>
2015-09-13 19:40:58 +0800

    iselect

--- iselect/js/iselect.js
+++ iselect/js/iselect.js
@@ -1,9 +1,13 @@
 ﻿var ISelect = function (option) {
-    var options = option.options,lis = "";
-    for (var i = 0, len = options.length; i < len; i++) {
+    var options = option.options,
+        lis = "",
+        parent = document.body,
+        i = 0,
+        len = options.length;
+    for (; i < len; i++) {
         lis += '<li>' + options[i].text + '</li>'
     }
-    document.querySelector(option.renderTo).insertAdjacentHTML("beforeEnd",
+    parent.insertAdjacentHTML("beforeEnd",
                         ' <div class="iselect">\
                                 <div class="iselect-toolbar"><a class="iselect-toolbar-ok">完成</a></div>\
                                 <div class="iselect-options">\
@@ -12,9 +16,11 @@
                                     <div class="iselect-mask2"></div>\
                                 </div>\
                             </div>');
-
-    var scroll = document.querySelector(".iselect-scroll"),
-        warpper = document.querySelector(".iselect-options"),
+    
+    var container = parent.querySelectorAll(".iselect")[0],
+        scroll = container.querySelector(".iselect-scroll"),
+        warpper = container.querySelector(".iselect-options"),
+        okBtn = container.querySelector(".iselect-toolbar-ok"),
         preY,
         logs = [],
         speed = 0,
@@ -26,13 +32,20 @@
         maxTop = parseInt(window.getComputedStyle(scroll).height) - parseInt(window.getComputedStyle(warpper).height) + minTop,
         minEaseTop = step * 4,
         maxEaseTop = maxTop + 2 * step;
+    css(scroll, 'top',2 * step - option.selectedIndex * step);
+
+    okBtn.addEventListener("click", function () {
+        this.hide();
+        var index = getSelectedIndex();
+        option.complete.call(this, option.options[index], index);
+    }.bind(this), false);
 
     warpper.addEventListener("touchmove", function (evt) {
         var dy = evt.touches[0].pageY - preY;
         preY = evt.touches[0].pageY;
         var positionY = (parseFloat(window.getComputedStyle(scroll).top) + dy);
         if (positionY < minEaseTop && Math.abs(positionY) < maxEaseTop) {
-            scroll.style.top = positionY + "px";
+            css(scroll, 'top',positionY);
         }
         logs.unshift({ time: new Date().getTime(), y: preY });
         evt.preventDefault();
@@ -51,11 +64,11 @@
     warpper.addEventListener("touchend", function (evt) {
         var positionY = parseInt(window.getComputedStyle(scroll).top);
         if (positionY > minTop) {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * minTop, 400);
+            to(scroll, 'top', (positionY < 0 ? -1 : 1) * minTop, 400);
             return;
         }
         if (Math.abs(positionY) > maxTop) {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * maxTop, 400);
+            to(scroll, 'top', (positionY < 0 ? -1 : 1) * maxTop, 400);
             return;
         }
         var now = new Date().getTime();
@@ -66,7 +79,7 @@
             if (Math.abs(speed) > 30) speed = (speed < 0 ? -1 : 1) * 30;
             initLoop();
             //  }else{
-            //   checkEnd();
+            //     correction();
             //  }
         }
         evt.preventDefault();
@@ -76,9 +89,10 @@
         loop = setInterval(function () {
             var positionY = (parseInt(window.getComputedStyle(scroll).top) + speed);
             if (positionY <= minEaseTop && Math.abs(positionY) < maxEaseTop) {
-                scroll.style.top = positionY + "px";
+                css(scroll,'top', positionY);
             }
             if (positionY > minTop || Math.abs(positionY) > maxTop) {
+                //当超出上下边界，摩擦力大一些
                 fx = 2;
             } else {
                 fx = 1.1;
@@ -86,45 +100,46 @@
             speed /= fx;
             if (Math.abs(speed) < 0.1) {
                 clearInterval(loop);
-                checkEnd();
+                correction();
             }
         }, 16)
     }
 
-    function checkEnd() {
+    function correction() {
         var positionY = parseInt(window.getComputedStyle(scroll).top);
         if (positionY > minTop) {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * minTop, 400);
+            to(scroll,'top', (positionY < 0 ? -1 : 1) * minTop, 400);
             return;
         }
         if (Math.abs(positionY) > maxTop) {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * maxTop, 400);
+            to(scroll, 'top', (positionY < 0 ? -1 : 1) * maxTop, 400);
             return;
         }
         var rpt = Math.floor(Math.abs(positionY / step));
         var dy = positionY % step;
         if (Math.abs(dy) > step / 2) {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * (rpt + 1) * step, 400);
+            to(scroll, 'top', (positionY < 0 ? -1 : 1) * (rpt + 1) * step, 400);
             // scroll.style.top = -1*(rpt+1)*step + "px";
         } else {
-            toTop(scroll, (positionY < 0 ? -1 : 1) * rpt * step, 400);
+            to(scroll, 'top', (positionY < 0 ? -1 : 1) * rpt * step, 400);
             //scroll.style.top = -1*rpt*step + "px";
         }
     }
 
-    function toTop(el, top, time) {
-        var dTop = top - parseInt(window.getComputedStyle(scroll).top);
-        var current = parseInt(window.getComputedStyle(scroll).top);
+    function to(el, name,value, time,callback) {
+        var dv = value - parseInt(window.getComputedStyle(el)[name]);
+        var current = parseInt(window.getComputedStyle(el)[name]);
         var start = new Date();
         checkLoop = setInterval(function () {
             var dt = new Date() - start;
             if (dt >= time) {
                 clearInterval(checkLoop);
-                el.style.top = top + 'px';
+                css(el, name, value);
+                callback&&callback();
                 return;
             }
-            var val = dTop * iosEase(dt / time) + current;
-            el.style.top = val + 'px';
+            var val = dv * iosEase(dt / time) + current;
+            css(el,name, val);
         }, 16);
 
 
@@ -133,5 +148,29 @@
     //http://kmdjs.github.io/dnt/demo43/index.html
     function iosEase(x) {
         return Math.sqrt(1 - Math.pow(x - 1, 2));
+    }
+
+    function css(element,name,value) {
+        element.style[name] = value + "px";
+    }
+
+    function getSelectedIndex() {
+        //60  30  0   -30   -60...   -300
+       var rpt= (step*2- parseInt(window.getComputedStyle(scroll).top))/30;
+       if (rpt < 0) return 0;
+       if (rpt > option.options.length) return option.options.length - 1;
+       return Math.round(rpt);
+    }
+
+    this.show = function () {
+        container.style.visibility = "visible";
+        container.style.display = "block";
+        to(container, 'bottom', 0, 400);
+    }
+
+    this.hide = function () {
+        to(container, 'bottom', -190, 400, function () {
+            container.style.display = "none";
+        });
     }
 }
